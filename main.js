7a1f0840b6f9ed043109d031ca82eee0cc1a1880
Mahdi Ben Mansour <kelkor664455@gmail.com>
2023-02-02 12:00:00 +0100

    fixed conflicts

---
+++ main.js
@@ -0,0 +1,10 @@
+const canvas=document.getElementById('carCanvas');
+canvas.height=window.innerHeight;
+canvas.width=200;
+
+const ctx = canvas.getContext('2d');
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+// can you point towards a part of the code if i tell you what it does ?
+
