74cf06b69d6db0fd6a762d42e2b9573219c545c2
Mahdi Ben Mansour <kelkor664455@gmail.com>
2023-02-07 10:00:00 +0100

    added logic to save best car's brain for later use

--- main.js
+++ main.js
@@ -8,12 +8,20 @@ const networkCtx = networkCanvas.getContext("2d");
 const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 const N = 100;
 const cars=generateCars(N);
+let bestCar=cars[0];
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"AI", 2),
 ];
 
 animate();
 
+function save(){
+    localStorage.setItem("bestBrain",JSON.stringify(bestCar.brain));
+}
+
+function discard(){
+    localStorage.removeItem("bestBrain");
+}
 function generateCars(N){
     const cars=[];
     for(let i=1;i<=N;i++){
@@ -29,7 +37,7 @@ function animate(time){
     for(let i=0;i<cars.length;i++){
         cars[i].update(road.borders,traffic);
     }
-    const bestCar = cars.find(c=>c.y==Math.min(...cars.map(c=>c.y)));
+    bestCar = cars.find(c=>c.y==Math.min(...cars.map(c=>c.y)));
     carCanvas.height=window.innerHeight;
     networkCanvas.height=window.innerHeight;
 
