5124c16602ddf8e5671454e6db42c62fb0e0e9d7
Mahdi Ben Mansour <kelkor664455@gmail.com>
2023-02-08 00:01:46 +0100

    added camera focus mechanism to follow the best performing car

--- controls.js
+++ controls.js
@@ -1,47 +1,51 @@
-class Controls {
-    constructor() {
-        this.forward = false;
-        this.left = false;
-        this.right = false;
-        this.reverse = false;
-
-        this.#addKeyboardListeners();
-    }
-    #addKeyboardListeners() {
-        document.onkeydown = (event) => {
-            switch(event.key) { 
-                case 'ArrowUp':
-                    this.forward = true;
-                    break;
-                case 'ArrowLeft':
-                    this.left = true;
-                    break;
-                case 'ArrowRight':
-                    this.right = true;
-                    break;
-                case 'ArrowDown':
-                    this.reverse = true;
-                    break;
-            }
-            console.table(this);
+class Controls{
+    constructor(type){
+        this.forward=false;
+        this.left=false;
+        this.right=false;
+        this.reverse=false;
+        switch(type){
+            case "KEYS":
+                this.#addKeyboardListeners();
+                break;
+            case "AI":
+                this.forward=true;
+                break;
         }
+    }
 
-        document.onkeyup = (event) => {
-            switch(event.key) {
-                case 'ArrowUp':
-                    this.forward = false;
+    #addKeyboardListeners(){
+        document.onkeydown=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=true;
                     break;
-                case 'ArrowLeft':
-                    this.left = false;
+                case "ArrowRight":
+                    this.right=true;
                     break;
-                case 'ArrowRight':
-                    this.right = false;
+                case "ArrowUp":
+                    this.forward=true;
                     break;
-                case 'ArrowDown':
-                    this.reverse = false;
+                case "ArrowDown":
+                    this.reverse=true;
                     break;
             }
-            console.table(this);
+        }
+        document.onkeyup=(event)=>{
+            switch(event.key){
+                case "ArrowLeft":
+                    this.left=false;
+                    break;
+                case "ArrowRight":
+                    this.right=false;
+                    break;
+                case "ArrowUp":
+                    this.forward=false;
+                    break;
+                case "ArrowDown":
+                    this.reverse=false;
+                    break;
+            }
         }
     }
 }
--- main.js
+++ main.js
@@ -1,18 +1,53 @@
-const canvas=document.getElementById('carCanvas');
-canvas.height=window.innerHeight;
-canvas.width=200;
+const carCanvas=document.getElementById("carCanvas");
+carCanvas.width=200;
+const networkCanvas=document.getElementById("networkCanvas");
+networkCanvas.width=400;
 
-const ctx = canvas.getContext('2d');
-const car = new Car(100,100,30,50);
-car.draw(ctx);
-
-// can you point towards a part of the code if i tell you what it does ?
+const carCtx = carCanvas.getContext("2d");
+const networkCtx = networkCanvas.getContext("2d");
+const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
+const N = 100;
+const cars=generateCars(N);
+const traffic=[
+    new Car(road.getLaneCenter(1),-100,30,50,"AI", 2),
+];
 
 animate();
 
-function animate() {
-    car.update();
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    car.draw(ctx);
+function generateCars(N){
+    const cars=[];
+    for(let i=1;i<=N;i++){
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"NeuralNetwork", 4));
+    }
+    return cars;
+}
+
+function animate(time){
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].update(road.borders,[]);
+    }
+    for(let i=0;i<cars.length;i++){
+        cars[i].update(road.borders,traffic);
+    }
+    const bestCar = cars.find(c=>c.y==Math.min(...cars.map(c=>c.y)));
+    carCanvas.height=window.innerHeight;
+    networkCanvas.height=window.innerHeight;
+
+    carCtx.save();
+    carCtx.translate(0,-bestCar.y+carCanvas.height*0.7);
+
+    road.draw(carCtx);
+    for(let i=0;i<traffic.length;i++){
+        traffic[i].draw(carCtx,"red");
+    }
+    carCtx.globalAlpha=0.2;
+    for(let i=0;i<cars.length;i++){
+        cars[i].draw(carCtx,"blue");
+    }
+    carCtx.globalAlpha=1;
+    bestCar.draw(carCtx,"blue",true);
+    carCtx.restore();
+    networkCtx.lineDashOffset=-time/50;
+    Visualizer.drawNetwork(networkCtx,bestCar.brain);
     requestAnimationFrame(animate);
-}   
+}
