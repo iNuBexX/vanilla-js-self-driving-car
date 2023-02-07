63e6b73c0481cc820a8ffe52390f72cd3ee0a4d4
Mahdi Ben Mansour <kelkor664455@gmail.com>
2023-02-07 10:30:00 +0100

    added more challenging traffic for testing AI performance

--- main.js
+++ main.js
@@ -9,8 +9,26 @@ const road=new Road(carCanvas.width/2,carCanvas.width*0.9);
 const N = 100;
 const cars=generateCars(N);
 let bestCar=cars[0];
+if(localStorage.getItem("bestBrain")){
+    for(let i=0;i<cars.length;i++){
+        cars[i].brain=JSON.parse(localStorage.getItem("bestBrain"));
+        if(i!=0){
+            NeuralNetwork.mutate(cars[i].brain,0.2);
+        }  
+    }
+}
 const traffic=[
     new Car(road.getLaneCenter(1),-100,30,50,"AI", 2),
+    new Car(road.getLaneCenter(0),-300,30,50,"AI", 2),
+    new Car(road.getLaneCenter(2),-300,30,50,"AI", 2),
+    new Car(road.getLaneCenter(1),-100,30,50,"AI", 2),
+    new Car(road.getLaneCenter(0),-500,30,50,"AI", 2),
+    new Car(road.getLaneCenter(2),-500,30,50,"AI", 2),
+    new Car(road.getLaneCenter(1),-700,30,50,"AI", 2),
+    new Car(road.getLaneCenter(2),-700,30,50,"AI", 2),
+    new Car(road.getLaneCenter(0),-900,30,50,"AI", 2),
+    new Car(road.getLaneCenter(1),-1300,30,50,"AI", 2),
+    new Car(road.getLaneCenter(2),-1300,30,50,"AI", 2),
 ];
 
 animate();
@@ -25,7 +43,7 @@ function discard(){
 function generateCars(N){
     const cars=[];
     for(let i=1;i<=N;i++){
-        cars.push(new Car(road.getLaneCenter(1),100,30,50,"NeuralNetwork", 4));
+        cars.push(new Car(road.getLaneCenter(1),100,30,50,"NeuralNetwork", 8));
     }
     return cars;
 }
